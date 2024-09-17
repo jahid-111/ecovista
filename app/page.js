7fafd9709fa7a7592c0dece5589e4adc466541fd
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-09-17 15:16:56 +0300

    Suspense_boundary[CSR]

--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
 import LocationDetector from "@/components/LocationDetector";
+import { Suspense } from "react";
 
 export default function Home() {
-  return <LocationDetector />;
+  return (
+    <Suspense
+      fallback={
+        <h3
+          className=" text-5xl h-screen text-red-500 font-semibold
+         bg-gray-200 p-20"
+        >
+          Loading...
+        </h3>
+      }
+    >
+      <LocationDetector />
+    </Suspense>
+  );
 }
